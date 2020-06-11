b4165f86d5b41dd6b033b25219a1ab3d7badab13
Cecil Symes <57969872+CCP115@users.noreply.github.com>
2020-06-11 13:42:58 +1200

    Update Item.java

--- app/src/main/java/com/compsys/pcpartmart/Item.java
+++ app/src/main/java/com/compsys/pcpartmart/Item.java
@@ -3,12 +3,16 @@ package com.compsys.pcpartmart;
 public class Item {
     private String name;
     private String price;
-    private int[] images;
+    private int image1;
+    private int image2;
+    private int image3;
 
-    public Item(String name, String price, int[] images) {
+    public Item(String name, String price, int image1, int image2, int image3) {
         this.name = name;
         this.price = price;
-        this.images = images;
+        this.image1 = image1;
+        this.image2 = image2;
+        this.image3 = image3;
     }
 
     public String get_name() {
@@ -20,6 +24,11 @@ public class Item {
     }
 
     public int get_image(int i) {
-        return images[i];
+        if (i == 2) {
+            return image2;
+        } else if (i == 3) {
+            return image3;
+        }
+        return image1;
     }
 }
